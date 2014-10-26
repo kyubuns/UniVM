be4311678fc1a68baa66a4f0d3e5ce1b991dd400
kyubuns <jp@ex.cx>
2014-10-26 10:48:50 +0900

    UILabelTextBinding追加

---
+++ Unity/UILabelTextBinding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace UniVM
+{
+  [RequireComponent(typeof(UILabel))]
+  [AddComponentMenu("UniVM/Binding/UILabel/Text")]
+  public sealed class UILabelTextBinding : Binding<string, UILabel, StringProperty>
+  {
+    protected override string GetComponentValue(UILabel component)
+    {
+      return component.text;
+    }
+
+    protected override void UpdateComponent(UILabel component, string value)
+    {
+      component.text = value;
+    }
+  }
+}
